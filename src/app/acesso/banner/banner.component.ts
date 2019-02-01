3dae4f428fc52b49541c545b874338a7ad1ef1d6
Daniel Castro <daniel.tiago.castro@celfocus.com>
2019-02-01 19:29:39 +0000

    aula 252

---
+++ src/app/acesso/banner/banner.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit} from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Imagem } from './imagem.model';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css'],
+  animations: [
+      trigger('banner', [
+        state('escondido', style({
+          opacity: 0
+        })),
+        state('visivel', style({
+          opacity: 1
+        })),
+        transition('escondido <=> visivel', animate('3s ease-in'))
+      ])
+  ]
+})
+export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel'
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url : 'assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url : 'assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url : 'assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url : 'assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url : 'assets/banner-acesso/img_5.png' }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 5000)
+  }
+
+  public logicaRotacao(): void {
+    console.log(this.imagens)
+
+    //auxilia na exibicao da imagem seguinte
+    let idx: number
+
+    //ocultar imagem
+    for(let i:number = 0; i <= 4; i++){
+      if(this.imagens[i].estado === 'visivel'){
+        this.imagens[i].estado = 'escondido'
+
+        idx = i === 4 ? 0 : i + 1
+
+        break
+      }
+    }
+
+    //exibir proxima imagem
+    this.imagens[idx].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 5000)
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcessoComponent } from './acesso/acesso.component';
+import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcessoComponent,
+    BannerComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
